ed6dddb0325257ed0a3db496e1cb06268ec2bc53
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-05-04 17:09:39 +0200

    get rid of monitoring sccirpt

--- monitoringScript.js
+++ monitoringScript.js
@@ -1,19 +1,19 @@
-// monitoringScript.js
-import { EventEmitter } from "events";
+// // monitoringScript.js
+// import { EventEmitter } from "events";
 
-class MonitoringScript extends EventEmitter {
-  constructor() {
-    super();
+// class MonitoringScript extends EventEmitter {
+//   constructor() {
+//     super();
 
-    // this.interval = setInterval(() => {
-    //   const data = "aa";
-    //   this.emit("update", data); // Emit update event
-    // }, 1000);
-  }
+//     // this.interval = setInterval(() => {
+//     //   const data = "aa";
+//     //   this.emit("update", data); // Emit update event
+//     // }, 1000);
+//   }
 
-  stop() {
-    clearInterval(this.interval);
-  }
-}
+//   stop() {
+//     clearInterval(this.interval);
+//   }
+// }
 
-export default new MonitoringScript();
+// export default new MonitoringScript();
