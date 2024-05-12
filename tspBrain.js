850b5ac50e3b14bb1b8562170271b0a42a54e2a6
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-05-12 18:14:46 +0200

    implement genetic algo

--- tspBrain.js
+++ tspBrain.js
@@ -9,6 +9,7 @@ export class TSP {
     this.parcels = parcels;
     this.x = playerPosition.x;
     this.y = playerPosition.y;
+    this.avgs = {};
 
     // this.plan = this.createPlan(parcelsQueue)
   }
@@ -170,13 +171,6 @@ export class TSP {
     //this.printMat(costs);
     return [costs, prep_parcels];
   }
-  sort_by_key(array, key) {
-    return array.sort(function (a, b) {
-      var x = a[key];
-      var y = b[key];
-      return x < y ? -1 : x > y ? 1 : 0;
-    });
-  }
 
   bruteTSP(costs, nodes) {
     console.log("Brute forcing TSP...");
@@ -285,6 +279,249 @@ export class TSP {
     return [best_path, best_score];
   }
 
+  // getRandomMask(size, p) {
+  //   let mask = [];
+  //   for (let i = 0; i < size; i++) {
+  //     mask.push(Math.random() < p ? 1 : 0);
+  //   }
+  //   return mask;
+  // }
+
+  maskList(list, p) {
+    let num_del = list.length - Math.floor(Math.random() * list.length + 1);
+
+    for (let i = 0; i < num_del; i++) {
+      let idx = Math.floor(Math.random() * list.length);
+      list.splice(idx, 1);
+    }
+    return list;
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  rouletteWheel(population, costs, nodes) {
+    let scores = [];
+    let tot_fit = 0;
+    for (const dna of population) {
+      let fit = this.fitness(dna, costs, nodes);
+      scores.push(fit);
+      tot_fit += fit;
+    }
+
+    let chances = [];
+    for (const score of scores) {
+      let chance = Math.round((score / tot_fit) * scores.length);
+      chances.push(chance);
+    }
+
+    return [scores, chances];
+  }
+
+  getElites(population, scores, elite_rate) {
+    let mapped = scores.map((el, idx) => {
+      return { index: idx, score: el };
+    });
+
+    mapped = this.sort_by_key(mapped, "score");
+
+    let elites = [];
+    let num_elites = Math.round(mapped.length * elite_rate);
+    for (let i = 0; i < num_elites; i++) {
+      elites.push(mapped[mapped.length - i - 1].index);
+    }
+    // console.log("Scores: ", scores);
+    // console.log("Mapped: ", mapped);
+    // console.log("Elites: ", elites);
+
+    let elite_dna = [];
+    for (const idx of elites) {
+      elite_dna.push(population[idx]);
+    }
+    return elite_dna;
+  }
+
+  pickOne(population, chances) {
+    let val = Math.random();
+    let cum = 0;
+    let norm = [];
+
+    for (const chance of chances) {
+      cum += chance;
+    }
+    for (const chance of chances) {
+      norm.push(chance / cum);
+    }
+    // console.log("Val: ", val);
+    let idx = 0;
+    while (val > 0) {
+      val -= norm[idx];
+      idx++;
+    }
+    idx--;
+    // console.log("Norm: ", norm);
+    // console.log("Idx: ", idx);
+
+    return population[idx];
+  }
+
+  crossover(parentA, parentB) {
+    // console.log("Parent A: ", parentA);
+    // console.log("Parent B: ", parentB);
+
+    let seg_len = Math.floor(parentA.length / 2);
+
+    let start = Math.floor((parentA.length - seg_len) / 2);
+    let end = start + seg_len;
+
+    let a_segment = parentA.slice(start, end);
+    let head = [];
+    let tail = [];
+
+    for (const el of parentB) {
+      if (!a_segment.includes(el)) {
+        if (head.length <= parentA.length - seg_len) {
+          head.push(el);
+        } else if (head.length + tail.length < parentA.length - seg_len) {
+          tail.push(el);
+        } else {
+          break;
+        }
+      }
+    }
+
+    // console.log("seg_len: ", seg_len);
+    // console.log("Start: ", start);
+    // console.log("End: ", end);
+    // console.log("Segment: ", a_segment);
+    // console.log("Head: ", head);
+    // console.log("Tail: ", tail);
+
+    let childA = [].concat(head).concat(a_segment).concat(tail);
+
+    // console.log("Child A: ", childA);
+
+    return childA;
+  }
+  fitness(dna, costs, nodes) {
+    let rew = 0;
+    // console.log("start at node : ", dna[0]);
+    rew += nodes[dna[0]].rew - nodes[dna[0]].in_c;
+
+    for (let i = 1; i < dna.length; i++) {
+      // console.log("from node : ", dna[i - 1], " to node : ", dna[i]);
+      // console.log("costs: ", costs[dna[i - 1]][dna[i]]);
+      // console.log("reward: ", nodes[dna[i]].rew);
+      rew += nodes[dna[i]].rew - costs[dna[i - 1]][dna[i]];
+    }
+    rew += -nodes[dna[dna.length - 1]].out_c;
+
+    if (rew < 0) {
+      rew = 0;
+    }
+    //console.log("Fitness: ", rew);
+    return rew;
+  }
+
+  geneticTSP(
+    costs,
+    nodes,
+    pop_size = 100,
+    gen_num = 100,
+    mutation_rate = 0.1,
+    elite_rate = 0.5
+  ) {
+    let genes = Array.from(Array(nodes.length).keys());
+
+    //this.printMat(costs);
+    // console.log("Nodes: ", nodes);
+    // console.log("Genes: ", genes);
+
+    let best_dna = [];
+    let best_fit = 0;
+    let population = [];
+
+    let skip_rate = 0.2; //TODO: check if skip rate works okay or not
+
+    for (let i = 0; i < pop_size; i++) {
+      let order = genes.slice();
+      order = order.sort(() => Math.random() - 0.5);
+      let masked = this.maskList(order, skip_rate);
+
+      population.push(masked);
+    }
+
+    //console.log("Population: ", population);
+
+    let tot_fit = 0;
+    for (const dna of population) {
+      //console.log("DNA: ", dna);
+      tot_fit += this.fitness(dna, costs, nodes);
+    }
+    //console.log("Average fitness: ", tot_fit / pop_size);
+
+    for (let i = 0; i < gen_num; i++) {
+      let new_pop = [];
+      const [scores, chances] = this.rouletteWheel(population, costs, nodes);
+      let elites = this.getElites(population, scores, elite_rate);
+      new_pop = new_pop.concat(elites);
+      // console.log("Scores: ", scores);
+      // console.log("Chances: ", chances);
+      // console.log("Elite dna: ", elites);
+      for (let j = 0; j < pop_size - elites.length; j += 2) {
+        let parentA = this.pickOne(population, chances);
+        let parentB = this.pickOne(population, chances);
+
+        let childA = this.crossover(parentA, parentB);
+        let childB = this.crossover(parentB, parentA);
+
+        // console.log("Parent A: ", parentA);
+        // console.log("Parent B: ", parentB);
+        // console.log("Child A: ", childA);
+        // console.log("Child B: ", childB);
+        // new_pop.push(child);
+        new_pop.push(childA);
+        new_pop.push(childB);
+      }
+      // console.log("Population: ", population);
+
+      // console.log("New population: ", new_pop);
+
+      for (let j = 0; j < new_pop.length; j++) {
+        if (Math.random() < mutation_rate) {
+          let idxA = Math.floor(Math.random() * new_pop[j].length);
+          let idxB = Math.floor(Math.random() * new_pop[j].length);
+          let tmp = new_pop[j][idxA];
+          new_pop[j][idxA] = new_pop[j][idxB];
+          new_pop[j][idxB] = tmp;
+        }
+      }
+
+      population = new_pop;
+
+      tot_fit = 0;
+
+      for (const dna of population) {
+        let fit = this.fitness(dna, costs, nodes);
+        tot_fit += fit;
+
+        if (fit > best_fit) {
+          best_fit = fit;
+          best_dna = dna;
+          //console.log("New best fit: ", best_fit);
+        }
+      }
+
+      if (i % 10 == 0) {
+        //console.log("Gen " + i + " avg fitness: ", tot_fit / pop_size);
+        //console.log(population.length, " ", pop_size);
+      }
+    }
+
+    return [best_dna, best_fit];
+  }
+
   createPlan() {
     //let zones = this.field.getClosestDeliveryZones({ x: this.x, y: this.y });
     // const MUL = 10;
@@ -335,8 +572,39 @@ export class TSP {
 
     const [costs, parc] = this.builGraphInOut();
 
-    this.notTSP(costs, parc);
+    let ers = [0.1, 0.2, 0.3, 0.4, 0.5];
+    // for (const er of ers) {
+    //   this.avgs[er] = 0;
+    // }
+    for (const er of ers) {
+      const [best_path, best_fit] = this.geneticTSP(
+        costs,
+        parc,
+        100,
+        100,
+        0.01,
+        er
+      );
 
+      if (isNaN(this.avgs[er])) {
+        this.avgs[er] = 0;
+        this.avgs["cnt"] = 0;
+      }
+      this.avgs[er] += best_fit;
+      this.avgs["cnt"] += 1;
+      //console.log("Best fit ", er, " ", best_fit);
+    }
+    //console.log("Averages: ", this.avgs);
+    for (const er of ers) {
+      console.log(
+        "Average fit ",
+        er,
+        " ",
+        Math.round(this.avgs[er] / this.avgs["cnt"])
+      );
+    }
+    //console.log("Averages: ", this.avgs);
+    console.log("-----------------------------");
     return [];
   }
 
