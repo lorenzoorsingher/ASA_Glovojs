49084e77f045279f174118900f5aa237f553151d
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-08-29 17:23:42 +0200

    if no plan found return empty list

--- planner/bfs_pddl.js
+++ planner/bfs_pddl.js
@@ -119,7 +119,7 @@ export async function bfs_pddl(couplesInput, blocking_agents) {
     // console.log("PDDL result:", pddlResult);
     if (!pddlResult || !Array.isArray(pddlResult) || pddlResult.length === 0) {
       console.log("No valid paths found by PDDL solver");
-      return couples.map(() => ({ path: -1 }));
+      return [];
     }
 
     let paths = couples.map((couple, index) => {
