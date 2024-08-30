7d3ca611d8c400045aef0f05d61dc1c165eb443e
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-08-30 11:22:23 +0200

    implement caching for inverse paths

--- planner/plan_cacher.js
+++ planner/plan_cacher.js
@@ -4,6 +4,7 @@ export class PlansCache {
   constructor(max_size = -1) {
     this.paths_cache = new Map();
     this.cache_hits = 0;
+    this.cache_hits_inv = 0;
     this.cache_misses = 0;
   }
 
@@ -23,30 +24,59 @@ export class PlansCache {
       "_" +
       blocking.join("_");
 
-    return entry;
+    let entry_inv =
+      Position.serialize(endTile) +
+      "_" +
+      Position.serialize(startTile) +
+      "_" +
+      blocking.join("_");
+
+    return [entry, entry_inv];
   }
 
   getEntry(startTile, endTile, blocking_agents) {
-    let entry = this.getCacheID(startTile, endTile, blocking_agents);
+    let [entry, entry_inv] = this.getCacheID(
+      startTile,
+      endTile,
+      blocking_agents
+    );
 
     // check if the path is already in the cache
     if (this.paths_cache.has(entry)) {
       this.cache_hits += 1;
 
       return this.paths_cache.get(entry);
-    } else {
-      this.cache_misses += 1;
     }
+
+    if (this.paths_cache.has(entry_inv)) {
+      this.cache_hits += 1;
+      this.cache_hits_inv += 1;
+      return this.paths_cache.get(entry_inv).slice().reverse();
+    }
+
+    this.cache_misses += 1;
     return -1;
   }
 
   setEntry(startTile, endTile, blocking_agents, path) {
-    let entry = this.getCacheID(startTile, endTile, blocking_agents);
+    let [entry, entry_inv] = this.getCacheID(
+      startTile,
+      endTile,
+      blocking_agents
+    );
+
     this.paths_cache.set(entry, path);
+
+    let inv_path = -1;
+    if (path != -1) {
+      inv_path = path.slice().reverse();
+    }
+    this.paths_cache.set(inv_path);
   }
 
   printMetrics() {
     console.log("[Cache]hits: ", this.cache_hits);
+    console.log("[Cache]hits_inv: ", this.cache_hits_inv);
     console.log("[Cache] size: ", this.paths_cache.size);
     console.log(
       "[Cache] hit rate: ",
